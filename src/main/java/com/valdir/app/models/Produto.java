cc65a62ead2759269ed63f42051f76ced10404b4
Valdir Cezar <valdircezar312@gmail.com>
2021-05-17 17:54:40 -0300

    initial instances of Produto

--- src/main/java/com/valdir/app/models/Produto.java
+++ src/main/java/com/valdir/app/models/Produto.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -13,6 +14,7 @@ import javax.validation.constraints.NotNull;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Produto {
 
     @Id
--- src/main/java/com/valdir/app/services/DBService.java
+++ src/main/java/com/valdir/app/services/DBService.java
@@ -1,6 +1,8 @@
 package com.valdir.app.services;
 
+import com.valdir.app.models.Produto;
 import com.valdir.app.models.Usuario;
+import com.valdir.app.repositories.ProdutoRepository;
 import com.valdir.app.repositories.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,10 +15,19 @@ public class DBService {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
     public void instanciaDB() {
         Usuario u1 = new Usuario(null, "Valdir Cezar", "488.484.130-13", "valdir@mail.com", "123");
         Usuario u2 = new Usuario(null, "João Pedro", "227.235.780-62", "joao@mail.com", "123");
 
+        Produto p1 = new Produto(null, "SmarTV", "SmarTV OLED");
+        Produto p2 = new Produto(null, "Celular", "Samsung A21s");
+        Produto p3 = new Produto(null, "Óculos", "Óculos de descanso");
+        Produto p4 = new Produto(null, "Notebook", "Dell Ultra power megatom");
+
         usuarioRepository.saveAll(Arrays.asList(u1, u2));
+        produtoRepository.saveAll(Arrays.asList(p1, p2, p3, p4));
     }
 }
