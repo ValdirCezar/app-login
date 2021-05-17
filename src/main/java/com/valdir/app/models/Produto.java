bffe0ddf32643c70eb023fd40b6bb3584489dc2a
Valdir Cezar <valdircezar312@gmail.com>
2021-05-17 17:35:25 -0300

    create Produto model

---
+++ src/main/java/com/valdir/app/models/Produto.java
@@ -0,0 +1,24 @@
+package com.valdir.app.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotNull(message = "Campo NOME é requerido")
+    private String nome;
+    private String descricao;
+}
