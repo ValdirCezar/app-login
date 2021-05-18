734049c9e16dc0e1b2e29708efcff9a61d44ff65
Valdir Cezar <valdircezar312@gmail.com>
2021-05-18 13:50:27 -0300

    add SecurityConfig

--- src/main/java/com/valdir/app/configurations/SecurityConfig.java
+++ src/main/java/com/valdir/app/configurations/SecurityConfig.java
@@ -1,23 +1,67 @@
 package com.valdir.app.configurations;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private static final String[] PUBLIC_MATCHERS = { "/h2-console/**" };
+    /**
+     * Environment é uma interface que representa o ambiente no qual
+     * o aplicativo atual está sendo executado. Ele pode ser usado
+     * para obter perfis e propriedades do ambiente do aplicativo.
+     */
+    @Autowired
+    private Environment environment;
+
+    private static final String[] PUBLIC_MATCHERS = {"/h2-console/**"};
+    private static final String[] PUBLIC_MATCHERS_GET = {"/produtos/**"};
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
+
+        /**
+         * Verificando os profiles ativos. Caso o perfil
+         * ativo contenha test o acesso ao h2 será liberado
+         */
+        if(Arrays.asList(environment.getActiveProfiles()).contains("test")) {
+            http.headers().frameOptions().disable();
+        }
+
+        // Habilita as configurações de cors e desabilita a proteção csrf
+        http.cors().and().csrf().disable();
         http.authorizeRequests()
                 .antMatchers(PUBLIC_MATCHERS).permitAll()
+                .antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
                 .anyRequest().authenticated();
+
+        // Assegurando que não será criada sessão de usuário
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    /**
+     * Configura o cors para receber requisições
+     * de multiplas fontes
+     *
+     * @return source
+     */
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
     }
 }
