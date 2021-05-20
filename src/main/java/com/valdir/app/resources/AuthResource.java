baa321b954abf39233fc1da0800ddb00c682986c
Valdir Cezar <valdircezar312@gmail.com>
2021-05-20 14:30:31 -0300

    refresh token

---
+++ src/main/java/com/valdir/app/resources/AuthResource.java
@@ -0,0 +1,28 @@
+package com.valdir.app.resources;
+
+import com.valdir.app.security.JWTUtil;
+import com.valdir.app.security.UserSS;
+import com.valdir.app.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthResource {
+
+    @Autowired
+    private JWTUtil jwtUtil;
+
+    @PostMapping(value="/refresh_token")
+    public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+        UserSS user = UserService.authenticated();
+        String token = jwtUtil.generateToken(user.getUsername());
+        response.addHeader("Authorization", "Bearer " + token);
+        return ResponseEntity.noContent().build();
+    }
+}
